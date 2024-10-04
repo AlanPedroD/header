e19f1f37268a530be495002cf926c17815923e72
Alan Dias <alandiaslp@hotmail.com>
2024-10-04 19:01:55 -0300

    nova cor de logo adicionada

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let botao = document.querySelector(".botao");
 // Array com os caminhos de todas as logos
 let logos = [
     "imagens/logo-01.png",
-    "imagens/logo-02.png" 
+    "imagens/logo-02.png", 
+    "imagens/logo-03.png" 
 ];
 
 // Variável para acompanhar qual logo está sendo exibida
